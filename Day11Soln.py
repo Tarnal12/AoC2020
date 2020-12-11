0047c15c6af610141a567b12d1850936f4bb1534
PIERS EYRE-WALKER <piers.eyre-walker@agilent.com>
2020-12-11 14:18:07 +0000

    Part 2, but it's garbage.

--- Day11Soln.py
+++ Day11Soln.py
@@ -10,43 +10,106 @@ class SeatingArea():
         self.grid = []
         for row in rows:
             self.grid.append([i for i in row])
+        self.add_outer_ring()
+
+    def add_outer_ring(self):
+        for row in self.grid:
+            row.insert(0, "E")
+            row.append("E")
+        blank_row = ["E" for i in range(self.width + 2)]
+        self.grid.insert(0, blank_row)
+        self.grid.append(blank_row)
 
     def run_step(self):
         new_grid = []
         for row in range(self.height):
             new_row = []
             for cell in range(self.width):
-                new_row.append(self.run_step_for_seat(row, cell))
+                new_row.append(self.run_step_for_seat(row + 1, cell + 1))
             new_grid.append(new_row)
         self.grid = new_grid
+        self.add_outer_ring()
         return self.grid                     
 
-    def get_surrounding_seats(self, row_num: int, col_num: int) -> Dict[str, int]:
-        cell_counts = {
-            "#": 0,
-            ".": 0,
-            "L": 0
-        }
-        with suppress(IndexError):
-            if row_num - 1 >= 0:
-                cell_counts[self.grid[row_num - 1][col_num + 0]] += 1
-                if col_num - 1 >= 0:
-                    cell_counts[self.grid[row_num - 1][col_num - 1]] += 1
-                if col_num + 1 < self.width:
-                    cell_counts[self.grid[row_num - 1][col_num + 1]] += 1
+    def get_visible_occupied_seats(self, row_num: int, col_num: int) -> int:
+        ne_found = False
+        n_found = False
+        nw_found = False
+        e_found = False
+        w_found = False
+        se_found = False
+        s_found = False
+        sw_found = False
+        occupied_chairs_seen = 0
+        scan_distance = 1
+        while sum([ne_found, n_found, nw_found, e_found, w_found, se_found, s_found, sw_found]) < 8:
+            if not ne_found:
+                tile = self.grid[row_num - scan_distance][col_num + scan_distance]
+                if tile == "#":
+                    occupied_chairs_seen += 1
+                    ne_found = True
+                elif tile in ["L", "E"]:
+                    ne_found = True
+                    
+            if not n_found:
+                tile = self.grid[row_num - scan_distance][col_num]
+                if tile == "#":
+                    occupied_chairs_seen += 1
+                    n_found = True
+                elif tile in ["L", "E"]:
+                    n_found = True
+                    
+            if not nw_found:
+                tile = self.grid[row_num - scan_distance][col_num - scan_distance]
+                if tile == "#":
+                    occupied_chairs_seen += 1
+                    nw_found = True
+                elif tile in ["L", "E"]:
+                    nw_found = True
 
-            if col_num - 1 >= 0:
-                cell_counts[self.grid[row_num + 0][col_num - 1]] += 1
-            if col_num + 1 < self.width:
-                cell_counts[self.grid[row_num + 0][col_num + 1]] += 1
+            if not e_found:
+                tile = self.grid[row_num][col_num + scan_distance]
+                if tile == "#":
+                    occupied_chairs_seen += 1
+                    e_found = True
+                elif tile in ["L", "E"]:
+                    e_found = True            
+                    
+            if not w_found:
+                tile = self.grid[row_num][col_num - scan_distance]
+                if tile == "#":
+                    occupied_chairs_seen += 1
+                    w_found = True
+                elif tile in ["L", "E"]:
+                    w_found = True
+
+            if not se_found:
+                tile = self.grid[row_num + scan_distance][col_num + scan_distance]
+                if tile == "#":
+                    occupied_chairs_seen += 1
+                    se_found = True
+                elif tile in ["L", "E"]:
+                    se_found = True
+                    
+            if not s_found:
+                tile = self.grid[row_num + scan_distance][col_num]
+                if tile == "#":
+                    occupied_chairs_seen += 1
+                    s_found = True
+                elif tile in ["L", "E"]:
+                    s_found = True
+                    
+            if not sw_found:
+                tile = self.grid[row_num + scan_distance][col_num - scan_distance]
+                if tile == "#":
+                    occupied_chairs_seen += 1
+                    sw_found = True
+                elif tile in ["L", "E"]:
+                    sw_found = True
             
-            if row_num + 1 < self.height:            
-                cell_counts[self.grid[row_num + 1][col_num + 0]] += 1
-                if col_num - 1 >= 0:
-                    cell_counts[self.grid[row_num + 1][col_num - 1]] += 1            
-                if col_num + 1 < self.width:
-                    cell_counts[self.grid[row_num + 1][col_num + 1]] += 1
-        return cell_counts
+            scan_distance += 1
+
+        return occupied_chairs_seen
 
     def run_step_for_seat(self, row_num: int, col_num: int) -> bool:
         curr_value = self.grid[row_num][col_num]
@@ -54,16 +117,16 @@ class SeatingArea():
             return curr_value
         
         if curr_value == "L":
-            return "#" if self.get_surrounding_seats(row_num, col_num)["#"] == 0 else "L"
+            return "#" if self.get_visible_occupied_seats(row_num, col_num) == 0 else "L"
 
         if curr_value == "#":
-            return "L" if self.get_surrounding_seats(row_num, col_num)["#"] >= 4 else "#"
+            return "L" if self.get_visible_occupied_seats(row_num, col_num) >= 5 else "#"
 
     def pretty_string(self) -> str:
         as_str = ""
         for row in self.grid:
             as_str += " ".join(row) + "\n"
-        return as_str
+        return as_str.replace('E', '')
 
     def occupied_seats(self) -> int:
         total = 0
